3c6c8280e4715d3f6ab744681497f8d3450be7b8
github2k20 <priyansh.44rastogi@gmail.com>
2021-11-19 13:07:34 +0530

    Made the test more generic - dynamic certifier address

--- x/cert/client/cli/query_test.go
+++ x/cert/client/cli/query_test.go
@@ -5,19 +5,57 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/certikfoundation/shentu/v2/x/cert/types"
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/suite"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	sdksimapp "github.com/cosmos/cosmos-sdk/simapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	"github.com/certikfoundation/shentu/v2/simapp"
+	"github.com/certikfoundation/shentu/v2/x/cert/keeper"
+	"github.com/certikfoundation/shentu/v2/x/cert/types"
+)
+
+var (
+	acc1 = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	acc2 = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	acc3 = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	acc4 = sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
 )
 
 type IntegrationTestSuite struct {
 	suite.Suite
+	app     *simapp.SimApp
+	ctx     sdk.Context
+	keeper  keeper.Keeper
+	address []sdk.AccAddress
 }
 
 func (suite *IntegrationTestSuite) SetupSuite() {
 	suite.T().Log("Setting up integration test suite")
+	suite.app = simapp.Setup(false)
+	suite.ctx = suite.app.BaseApp.NewContext(false, tmproto.Header{})
+	suite.keeper = suite.app.CertKeeper
 
+	for _, acc := range []sdk.AccAddress{acc1, acc2, acc3, acc4} {
+		err := sdksimapp.FundAccount(
+			suite.app.BankKeeper,
+			suite.ctx,
+			acc,
+			sdk.NewCoins(
+				sdk.NewCoin("uctk", sdk.NewInt(10000000000)), // 1,000 CTK
+			),
+		)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	suite.address = []sdk.AccAddress{acc1, acc2, acc3, acc4}
+	suite.keeper.SetCertifier(suite.ctx, types.NewCertifier(suite.address[0], "", suite.address[0], ""))
 }
 
 func (suite *IntegrationTestSuite) TestCertifierQueryCmd() {
@@ -34,7 +72,7 @@ func (suite *IntegrationTestSuite) TestCertifierQueryCmd() {
 	}{
 		{
 			"valid query",
-			"certik18w5txca7skklxe7y54nsxz22tqgtvrkzffrem8",
+			suite.address[0].String(),
 			errArgs{
 				shouldPass: true,
 				contains:   "",
